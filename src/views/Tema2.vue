afe4a764b72a71b7e5204908dfb6ba824cfb3888
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-08-09 19:24:15 -0500

    avance

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -27,6 +27,234 @@
       Separador
       #t_2_1.titulo-segundo.color-acento-contenido
         h2 2.1 Concepto de mercado
+      
+      .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
+        .col-lg-8
+          .bloque-texto-b.color-secundario.p-4
+            .bloque-texto-b__texto
+              i.fas.fa-quote-left
+              p De acuerdo con Patricio Bonta y Mario Farber (2002), en su libro “199 Preguntas Sobre Marketing y Publicidad”, el mercado es:<br><br>
+              p Donde confluyen la oferta y la demanda. En un sentido menos amplio, el mercado es el conjunto de todos los compradores reales y potenciales de un producto. Por ejemplo: el mercado de los autos está formado no solamente por aquellos que poseen un automóvil, sino también por quienes estarían dispuestos a comprarlo y disponen de los medios para pagar su precio. (pág. 19)<br><br>
+              h5 - Patricio Bonta y Mario Farber (2002)
+              i.fas.fa-quote-right
+        .col-lg-4
+          img(src="@/assets/curso/temas/tema2/img02.png", alt="")
+      .py-3 
+        .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
+          .col-lg-12
+            .bloque-texto-f.color-secundario.pt-4.px-5.mb-5
+              .bloque-texto-f__comillas
+                i.fas.fa-quote-left
+                i.fas.fa-quote-right
+              p.text-regular.bloque-texto-f__texto Por su parte, Philip Kotler, Gary Armstrong, Dionisio Cámara e Ignacio Cruz (2012), en su libro “Marketing”, indican que el mercado es un “Conjunto de compradores reales y potenciales de un producto. Estos compradores comparten una necesidad o un deseo particular que puede satisfacerse mediante una relación de intercambio”. (pág.10)
+              h5.text-regular.bloque-texto-f__texto #[b - Philip Kotler, Gary Armstrong, Dionisio Cámara e Ignacio Cruz  (2012)]
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-5
+            img(src="@/assets/curso/temas/tema2/img03.png", alt="")
+          .col-lg-7
+            p En un sentido más amplio de la palabra mercado y desde el punto de vista de la empresa, se podría decir que es el conjunto de todas las variables que interfieren en el desarrollo comercial de los productos y servicios; teniendo en cuenta que confluyen todos los actores del proceso de mercadeo y los factores que obligan a las empresas a desarrollar diferentes estrategias para permanecer en él y ser competitivas.
+
+      Separador
+      #t_2_2.titulo-segundo.color-acento-contenido
+        h2 2.2 Proceso de segmentación
+
+      p De acuerdo con las teorías de la segmentación, existen varios tipos: geográfica, demográfica, comportamental, psicográfica, por uso del producto, por comportamiento de compra, entre otras. En el proceso que se relaciona a continuación, se presentan los pasos esenciales para la segmentación de mercados industriales y de consumidores, de forma integral; esto, con el fin de que quien desarrolle las estrategias, tenga la capacidad de aplicarlos en su totalidad o por partes de acuerdo con sus exigencias:
+      
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+          .col-sm-12.col-lg-7
+            .d-flex.align-items-center.mt-4
+              img.w-card-icon(src="@/assets/curso/temas/tema2/ico1.svg")
+              h3.bg13.py-2.w-card-text Segmentación industrial
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-3
+          img(src="@/assets/curso/temas/tema2/img04.png", alt="")
+        .col-lg-9
+          p Muchas empresas tienen dentro de su portafolio productos o servicios, los cuales tienen como mercado objetivo otras empresas y su método de negociación incluye el B2B (Business to Business) o negocio a negocio. Por ejemplo, para un fabricante de máquinas de coser cuero, su mercado objetivo son fábricas de artículos de este material y no enfoca sus estrategias en el de consumidores.
+          .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+            .col-sm-12.col-lg-7
+              .d-flex.align-items-center.mt-4
+                img.w-card-icon(src="@/assets/curso/temas/tema2/ico2.svg")
+                h3.bg13.py-2.w-card-text Pasos para la segmentación industrial
+              .py-2
+              p El proceso de la segmentación industrial es relativamente sencillo y la caracterización de las empresas incluye solo algunas variables muy puntuales. Los pasos para su aplicación son:
+
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-8
+          h4 Selección del universo
+          p Uno de los tipos de segmentación más importantes es la geográfica. Este primer paso, permite deli  mitar el área de cobertura que tendrá el producto o servicio desde el punto de vista geográfico. El universo, es toda la zona donde hacen presencia los clientes y la empresa está en capacidad de llegar allí; por ejemplo, si una compañía vende harina industrial y sus clientes son fabricantes de alimentos, estos pueden estar en todo el país y hacer entregas del producto por un servicio de courier. En ese caso, el universo es Colombia, sin importar que su fabricación y distribución esté en el Valle del Cauca.
+        .col-lg-4
+          img(src="@/assets/curso/temas/tema2/img05.png", alt="")
+      .row
+        .col-lg-10
+          h4 Caracterización del mercado
+          p La caracterización del mercado se realiza mediante variables; para el caso de la segmentación industrial, se tendrán en cuenta las siguientes:
+      
+      .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
+        .col-lg-1
+          img(src="@/assets/curso/temas/tema2/ico3.svg", alt="")
+        .col-lg-11
+          h5 Ubicación geográfica
+          p Si bien, ya se precisó el universo del segmento del mercado, es importante definir en qué áreas de esa zona están ubicados los clientes. Por ejemplo, si el universo es Colombia, en este caso se puede detallar como ubicación geográfica, las capitales de los departamentos.
+
+        .col-lg-1
+          img(src="@/assets/curso/temas/tema2/ico4.svg", alt="")
+        .col-lg-11
+          h5 Tamaño de la empresa
+          p De acuerdo con el Ministerio de Comercio, Industria y Turismo, en Colombia existen cuatro tamaños de empresas, definidos por su actividad económica y por los ingresos que generan: 
+
+      .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
+        .col-lg-11
+          SlyderF.mb-5(columnas="col-lg-6 col-xl-4")(data-aos="zoom-in-left")
+            .tarjeta.color-secundario-cus.p-4
+              .row.justify-content-center.mb-3
+                .col-sm-3.col-lg-5.my-5.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/tema2/ico5.svg')
+              .row.justify-content-center.mb-3
+                .col-lg-8.fondo-morado.p-1
+                  h5.text-center.text-white Manufactura
+                .py-2
+                ul.lista-ul
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Microempresas:] ingresos inferiores o iguales a $807'504.010.   
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Pequeñas empresas:]  ingresos superiores a $807’504.010 y menores o iguales a $7.025’178.65 
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Medianas empresas:] ingresos superiores a $7.025’178.650 e inferiores o iguales a $59.512’082.550.
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Grandes empresas:]  ingresos superiores al valor expresado anteriormente.     
+                      
+            .tarjeta.color-secundario-cus.p-4
+              .row.justify-content-center.mb-3
+                .col-sm-3.col-lg-5.my-5.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/tema2/ico6.svg')
+              .row.justify-content-center.mb-3
+                .col-lg-6.fondo-morado.p-1
+                  h5.text-center.text-white Servicios
+                .py-2
+                ul.lista-ul
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Microempresas:]  ingresos inferiores o iguales a $1.130’498.760  
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Pequeñas empresas:]   ingresos superiores a $1.130’498.760 e inferiores o iguales a $4.521’960.770.
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Medianas empresas:] ingresos superiores a $1.130’498.760 e inferiores o iguales a $16.553’575.180.
+
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Grandes empresas:]   ingresos superiores al valor expresado anteriormente.
+    
+            
+            .tarjeta.color-secundario-cus.p-4
+              .row.justify-content-center.mb-3
+                .col-sm-3.col-lg-5.my-5.d-none.d-lg-block
+                  img(src='@/assets/curso/temas/tema2/ico7.svg')
+              .row.justify-content-center.mb-3
+                .col-lg-6.fondo-morado.p-1
+                  h5.text-center.text-white Comercio
+                .py-2
+                ul.lista-ul
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Microempresas:] ingresos inferiores o iguales a $1.534’233.630. 
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Pequeñas empresas:] ingresos superiores a $1.534’233.630 e inferiores o iguales a $14.796’723.630.
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Medianas empresas:] ingresos superiores a $14.796’723.630 e inferiores o iguales a $74.046’914.840.
+
+                  li 
+                    i.fas.fa-check-circle
+                    | #[b Grandes empresas:] ingresos superiores al valor expresado anteriormente.
+
+
+      .row.justify-content-start.mb-4(data-aos="zoom-in-left")
+        .col-sm-12.col-lg-7
+          .d-flex.align-items-center.mt-4
+            img.w-card-icon(src="@/assets/curso/temas/tema2/ico8.svg")
+            h3.bg13.py-2.w-card-text Segmentación de consumidores
+      
+      .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+        .col-lg-12
+          p Es una de las herramientas más importantes para cualquier empresa, en el desarrollo de estrategias de comercialización, mercadeo y promoción. La caracterización de los clientes permite la satisfacción de las necesidades con el mínimo de fallas, pues conocer a detalle sus expectativas, se convierte en la información más importante en la generación de productos y servicios. Esta segmentación se realiza aplicando variables demográficas en dos fases diferentes, en la primera se determina el mercado potencial de la empresa y en la segunda el mercado real. Ambas, representan un alto nivel de importancia, debido a las posibilidades que otorgan para el desarrollo de las estrategias.
+      
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-4
+          .titulo-sexto.color-acento-contenido
+            h5 Figura 1.
+            span Proceso de segmentación
+      
+      .row.justify-content-center.align-items-center.mb-5
+        .col-lg-6
+          img(src='@/assets/curso/temas/tema2/figura1.svg', alt='La imagen ilustra el proceso de segmentación de mercado mediante un diagrama concéntrico que abarca desde el universo total hasta segmentos específicos. Comienza con el mercado potencial, definido por variables demográficas básicas y geográficas, y se estrecha hacia el mercado real, caracterizado por variables psicográficas y demográficas más detalladas. Dentro del mercado real, se identifican grupos específicos (Grupo 1, Grupo 2, Grupo 3) que representan segmentos de mercado precisos basados en comportamientos de compra y rangos demográficos reducidos.')
+
+      .row.justify-content-center.align-items-center.mb-5(data-aos="zoom-in-left")
+        .col-lg-3
+          img(src="@/assets/curso/temas/tema2/img06.png")
+
+        .col-lg-9
+          AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+            .row(titulo="Ubicación geográfica")
+              .col-md-12.mb-4.mb-md-0
+                p De la misma forma como se determinó el segmento industrial, en el de los consumidores también es necesario identificar un área geográfica donde la empresa desarrollará sus operaciones, este punto determinará el primer aspecto de potencialidad para su desarrollo comercial. Por ello, una vez definido el universo, es necesario hacerlo con las áreas específicas en que se llegará a los consumidores potenciales, debido a que en muchas ocasiones la cobertura total del universo es inalcanzable para la empresa. Por ejemplo, si se definió Antioquia como universo, la ubicación geográfica del mercado potencial puede abarcar los municipios del centro y del oriente del departamento.
+
+            .row(titulo="Sexo")
+              .col-md-12.mb-4.mb-md-0
+                p Esta variable define el sexo del mercado al que la empresa dirige el producto, muchos de ellos van direccionados a ambos (hombres y mujeres).
+            
+            .row(titulo="Edad")
+              .col-md-12.mb-4.mb-md-0
+                p En el mercado potencial, como ya se explicó, se deben incluir rangos amplios, incluyendo a todos aquellos posibles consumidores.
+
+            .row(titulo="Nivel socioeconómico")
+              .col-md-12.mb-4.mb-md-0
+                p Para el caso de Colombia, se determina a través de la estratificación que va de 1 a 6. En este caso, también se incluyen todos los posibles compradores y por ello, se aplican rangos amplios.
+
+
+        p Estas cuatro variables permiten conocer el total del mercado potencial de la empresa; además, este dato cuantitativo permite planear estrategias de ventas y proyecciones de mercadeo. Por este motivo, al momento de querer cuantificar el mercado, es indispensable acudir a los datos que proporciona el DANE.
+
+
+      .row.justify-content-center.mb-5
+        .col-lg-6
+          .titulo-sexto.color-secundario
+            h5 Tabla 1. 
+            span Proceso de segmentación – Bogotá
+          img(src='@/assets/curso/temas/tema2/tabla1.svg', alt='La tabla muestra la población de Bogotá, D.C. en 2020, dividida por grupos de edad y género, con un total de 7.743.955 personas: 3.707.833 hombres y 4.036.111 mujeres. Los datos están segmentados en intervalos de cinco años, desde 0-4 hasta 70-74 años. Por ejemplo, en el grupo de 0-4 años hay 493.287 personas (251.852 hombres y 241.435 mujeres), y en el grupo de 20-24 años hay 711.590 personas (354.946 hombres y 356.644 mujeres), proporcionando una visión detallada de la distribución etaria y de género en la ciudad. ')
+        .col-lg-6
+          .titulo-sexto.color-secundario
+            h5 Tabla 2. 
+            span Distribución de los hogares por estrato socioeconómico a nivel nacional 
+          img(src='@/assets/curso/temas/tema2/tabla2.svg', alt='El gráfico muestra la distribución de hogares por estrato socioeconómico a nivel nacional, indicando que la mayoría de los hogares se encuentran en los estratos 2 y 3. Específicamente, el 44.2% de los hogares pertenece al estrato 2, seguido del 27.1% en el estrato 3. Los estratos 1 y 4 representan el 22.3% y 6.3% de los hogares, respectivamente, mientras que los estratos 5 y 6 tienen una menor representación, con 1.9% y 1.2% de los hogares. En términos de millones de hogares, esto se refleja en una mayor concentración en los estratos medios y bajo ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 </template>
