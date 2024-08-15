d2a703ffaf6adc8dba6d09a4dfa2dfda5fbd3c94
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2024-08-15 16:17:45 -0500

    avance

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -8,7 +8,7 @@
         h1 El <i>brief</i>
 
       .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
-        .col-lg-4
+        .col-lg-4.col-8.col-sm-6
           img(src="@/assets/curso/temas/tema1/img01.svg", alt="")
         .col-lg-8
           p Cualquier proyecto que se desee emprender, independiente del campo en el que se vaya a desarrollar, requiere una información inicial con datos que permitan conocer todos sus pormenores, para que quienes lo realicen tengan claridad total al respecto y puedan tomar decisiones con base en factores argumentados, y así se minimicen al máximo los riesgos.
@@ -46,9 +46,9 @@
         figcaption Video: Leyenda del video
 
       .row.justify-content-center.align-items-center.mb-5
-        .col-lg-4
+        .col-lg-4.col-8.col-sm-4
           img(src="@/assets/curso/temas/tema1/img03.svg", alt="")
-        .col-lg-8
+        .col-lg-8.col-sm-7
           SlyderF.mb-5(columnas="col-lg-6 col-xl-6")(data-aos="zoom-in-left")
             .tarjeta.color-secundario-cus.p-4
               .row.justify-content-center.mb-3
@@ -186,7 +186,7 @@
 
 
         .row.justify-content-center.align-items-center.mb-5(data-aos="fade-left")
-          .col-lg-4
+          .col-lg-4.col-8.col-sm-6
             img(src='@/assets/curso/temas/tema1/img04.svg')
           .col-lg-8
             p Para este brief, al contrario del de primer contacto, su finalidad es el desarrollo estratégico de la comunicación de una empresa, para lograr un resultado en particular. Es importante tener en cuenta que una táctica es una pequeña parte de una estrategia; por lo tanto, cuando una empresa requiere de la implementación de un plan de comunicaciones, tendrá la necesidad de crear varios de ellos. También, cabe resaltar que este tipo de brief tiene un enfoque específico en un producto o servicio o en su defecto, en una línea de estos; es poco frecuente que sea funcional para aquellos que pertenecen a varias líneas, a no ser que la estrategia sean ofertas que los involucre en una misma campaña. 
@@ -209,7 +209,7 @@
           .col-md-6.mb-4.mb-md-0
             h5 Identificación de la campaña a realizar: 
             p se deben incluir los datos relevantes de la campaña para su identificación: ciudad, fecha, cliente, producto, campaña y contacto.
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img05.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Necesidad")
@@ -217,111 +217,111 @@
             p #[b  Necesidad de la marca:] hace referencia a la necesidad de comunicación de la empresa y se divide en los siguientes puntos.
             p se deben incluir los datos relevantes de la campaña para su identificación: ciudad, fecha, cliente, producto, campaña y contacto.
             .row
-              .col-md-2.mb-4.mb-md-0
+              .col-md-2.mb-4.mb-md-0.col-4
                 img(src="@/assets/curso/temas/tema1/law1.svg")
               .col-md-10.mb-4.mb-md-0
                 p #[b Antecedentes:] un punto muy importante, porque describe todos los pormenores de la marca, que finalmente son las razones por las que la empresa ha decidido realizar la campaña; aquí, se deben relacionar los historiales de ventas, las ofensivas de la competencia, los cambios de comportamiento del mercado, los altos niveles de participación, los cambios en las tendencias o cualquier otro factor que afecte de forma negativa o positiva las ventas o posicionamiento de la marca.
           
-              .col-md-2.mb-4.mb-md-0
+              .col-md-2.mb-4.mb-md-0.col-4
                 img(src="@/assets/curso/temas/tema1/law1.svg")
               .col-md-10.mb-4.mb-md-0
                 p #[b La necesidad de la marca:] una vez entendida la situación del producto o servicio, es necesario plantear con base en dicha información, qué requiere la marca en cuanto a la comunicación para superar los obstáculos planteados o mantenerse en una situación favorable, si es el caso.
 
-              .col-md-2.mb-4.mb-md-0
+              .col-md-2.mb-4.mb-md-0.col-4
                 img(src="@/assets/curso/temas/tema1/law1.svg")
               .col-md-10.mb-4.mb-md-0
                 p #[b Residual de la comunicación:] ya planteada la necesidad, es claro lo que se busca con la comunicación, este punto describe lo que la empresa busca que suceda una vez la campaña haya sido expuesta al mercado.
 
-              .col-md-2.mb-4.mb-md-0
+              .col-md-2.mb-4.mb-md-0.col-4
                 img(src="@/assets/curso/temas/tema1/law1.svg")
               .col-md-10.mb-4.mb-md-0
                 p #[b Descripción del proyecto:] gracias a la recolección y análisis de información relevante del producto, la empresa tiene las herramientas para la formulación de estrategias que permiten el aumento de ventas y posicionamiento de marca; en este punto deben exponerse para que la agencia de publicidad, asesor o freelancer sean capaces de comprenderlas e interpretarlas y les suministren recursos para la elaboración de la estrategia creativa.
 
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img06.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Objetivos")
           .col-md-6.mb-4.mb-md-0
             p #[b  Objetivos:] para el desarrollo de una campaña táctica, la empresa debe trazarse dos tipos de objetivos:
             .row
-              .col-md-2.mb-4.mb-md-0
+              .col-md-2.mb-4.mb-md-0.col-4
                 img(src="@/assets/curso/temas/tema1/law2.svg")
               .col-md-10.mb-4.mb-md-0
                 p #[b Objetivo de comunicación:] es cualitativo y su enfoque está en el actuar del mercado objetivo, en su pensamiento o en sus sentimientos frente al mensaje al que será expuesto.
 
-              .col-md-2.mb-4.mb-md-0
+              .col-md-2.mb-4.mb-md-0.col-4
                 img(src="@/assets/curso/temas/tema1/law3.svg")
               .col-md-10.mb-4.mb-md-0
                 p #[b Objetivo de medios:] se plantea desde un punto de vista más cuantitativo y se enfoca en la cantidad de personas pertenecientes al mercado meta que va a alcanzarse con la campaña.
           
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img07.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="<i>Target</i>")
           .col-md-6.mb-4.mb-md-0
               p #[em #[b  target:]] siendo la variable más importante del mercado, el target de la marca es el elemento principal a analizar en un brief para el desarrollo de la campaña. Para este punto, deben tenerse en cuenta dos partes fundamentales:
               .row
-                .col-md-2.mb-4.mb-md-0
+                .col-md-2.mb-4.mb-md-0.col-4
                   img(src="@/assets/curso/temas/tema1/law4.svg")
                 .col-md-10.mb-4.mb-md-0
                   p #[b Grupo objetivo:] es cualitativo y su enfoque está en el actuar del mercado objetivo, en su pensamiento o en sus sentimientos frente al mensaje al que será expuesto.
 
-                .col-md-2.mb-4.mb-md-0
+                .col-md-2.mb-4.mb-md-0.col-4
                   img(src="@/assets/curso/temas/tema1/law5.svg")
                 .col-md-10.mb-4.mb-md-0
                   p #[b #[em Target] comunicacional:]  también es conocido como arquetipo de cliente; es una descripción individualizada y detallada del consumidor del producto o usuario del servicio a nivel psicográfico y conductual. En algunos productos, es necesario realizar varios targets comunicacionales y del desarrollo de múltiples versiones de la campaña para llegar a varios nichos.
           
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img08.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Mensaje")
           .col-md-6.mb-4.mb-md-0
               p #[b  Mensaje principal a comunicar:] la razón principal por la que se hace un brief, es el desarrollo de un mensaje publicitario que luego será expuesto con imágenes, textos, videos, audios y la combinación de todos ellos. En este punto, la empresa toma las decisiones del mensaje a comunicar para que la agencia, asesor o freelancer publicitario las conviertan en conceptos creativos originales, llamativos y atractivos. Se divide en los siguientes elementos:
               .row
-                .col-md-2.mb-4.mb-md-0
+                .col-md-2.mb-4.mb-md-0.col-4
                   img(src="@/assets/curso/temas/tema1/law6.svg")
                 .col-md-10.mb-4.mb-md-0
                   p #[b Beneficio racional:] edescribe lo que el consumidor debería pensar una vez sea expuesto al mensaje de la campaña.
                 .py-2
-                .col-md-2.mb-4.mb-md-0
+                .col-md-2.mb-4.mb-md-0.col-4
                   img(src="@/assets/curso/temas/tema1/law7.svg")
                 .col-md-10.mb-4.mb-md-0
                   p #[b Beneficio emocional:] aquí se expresa lo que el consumidor debería sentir cuando el mensaje de la campaña lo impacte. Este elemento se convierte en la base de ello, teniendo en cuenta que los estudios han determinado que la decisión de compra se realiza principalmente por los sentimientos; es decir, en la adquisición de un bien, prima el beneficio emocional por encima del racional.
           
-                .col-md-2.mb-4.mb-md-0
+                .col-md-2.mb-4.mb-md-0.col-4
                   img(src="@/assets/curso/temas/tema1/law8.svg")
                 .col-md-10.mb-4.mb-md-0
                   p #[b #[em Reason to believe:]] conocida también como reason why (la razón del por qué). Se refiere al motivo por la que el consumidor va a decidirse por el producto que se está promocionando, por qué debe creer que es mejor que el de la competencia.
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img09.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Qué no debe")
           .col-md-6.mb-4.mb-md-0
             p #[b Qué no debe entenderse con el mensaje:] todo mensaje por claro y conciso que sea, puede llevar a confusiones y malentendidos. Por ello, este punto debe prever esos posibles baches y manifestar qué no debe creer el consumidor con la campaña o qué asociación viciosa debe evitarse con lo que se proyecte.
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img10.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Piezas")
           .col-md-6.mb-4.mb-md-0
             p #[b  Piezas a desarrollar:] de acuerdo con los medios planteados y con las versiones que deben hacerse de los anuncios, es necesario definir qué piezas se van a crear; adicional, es necesario relacionar:
               .row
-                .col-md-2.mb-4.mb-md-0
+                .col-md-2.mb-4.mb-md-0.col-4
                   img(src="@/assets/curso/temas/tema1/law9.svg")
                 .col-md-10.mb-4.mb-md-0
                   p #[b Guías de ejecución:] definen cómo se van a realizar las piezas anteriormente descritas: tipos de gráficos, colores predominantes, formatos, tamaños, tipografías, etc.
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img11.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Cronograma")
           .col-md-6.mb-4.mb-md-0
             p #[b Cronograma de trabajo:]  es donde el empresario determina cuándo debe ser entregada la campaña por parte de la agencia, el asesor o el #[em freelancer].
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img12.png")
         
         .row.justify-content-center.align-items-center.mb-5(titulo="Legales")
           .col-md-6.mb-4.mb-md-0
             p #[b Legales:] Se deben relacionar aquellos elementos que la empresa debe incluir por temas legales o éticos: textos de advertencia, logos de entidades de vigilancia, logos de la empresa, iconos o fuentes de derechos de autor o demás elementos que considere el empresario.
-          .col-md-4.mb-4.mb-md-0
+          .col-md-4.mb-4.mb-md-0.col-8
             img(src="@/assets/curso/temas/tema1/img13.png")
       
       p Una vez el #[em brief] sea diligenciado y entregado a la empresa, debe ser un documento disponible para que todos los creativos y ejecutivos de la agencia, puedan consultarlo cada vez que lo requieran.
